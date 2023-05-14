1061060eed11a0a33e5285db62edc54beaa5ce11
youshitsune <luka@youshitsune.me>
2023-05-14 08:58:45 +0200

    implement hightlighting for headers

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0 
+
+int 
+main(int argc, char *argv[]){
+    if (argc < 2) {
+        printf("HELP:");
+    } else if (argc > 2) {
+        printf("HELP:");
+    } else {
+        FILE *fp;
+        fp = fopen(argv[1], "r");
+        fseek(fp, 0, SEEK_END);
+        int file_size = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
+        char ctx[file_size];
+        int c = getc(fp);
+        int i = 1;
+        ctx[0] = c;
+        while (c != EOF){
+            c = getc(fp);
+            ctx[i] = c;
+            ++i;
+        }
+        int heading = FALSE;
+        for (int i = 0; i < file_size; i++){
+            if (heading == TRUE){
+                if (ctx[i] == '\n'){
+                    printf("%c", ctx[i]);
+                    heading = FALSE;
+                } else { 
+                    printf("\033[1;31m%c\033[0m", ctx[i]);
+                }
+            } 
+            else if (heading == FALSE){
+                if (ctx[i] == '#'){
+                    heading = TRUE;
+                    printf("\033[1;31m%c\033[0m", ctx[i]);
+                } else {
+                    printf("%c", ctx[i]);
+                }
+            }
+            
+            }
+        }
+        
+        
+}
+
